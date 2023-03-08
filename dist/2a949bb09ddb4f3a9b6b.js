3631e2079a6d26eb64d2eeb6b80969f848e51033
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-08 20:35:57 +0300

    next build

---
+++ dist/2a949bb09ddb4f3a9b6b.js
@@ -0,0 +1,1 @@
+import Character from"./character";import Bowman from"./bowman";import Swordsman from"./swordsman";import Magician from"./magician";import Undead from"./undead";import Zombie from"./zombie";import Daemon from"./daemon";
