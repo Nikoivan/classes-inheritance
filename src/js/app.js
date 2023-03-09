f79ca498708ad5d210e9e760b06783a15c476436
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-09 22:26:24 +0300

    lint correction

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
-import Bowman from './bowman';
-import Swordsman from './swordsman';
-import Magician from './magician';
-import Daemon from './daemon';
-import Undead from './undead';
-import Zombie from './zombie';
-/*
-const bowman = new Bowman('Ivan');
-const swordsman = new Swordsman('Yana');
-const magician = new Magician('Alexander');
-const daemon = new Daemon('Tanya');
-const undead = new Undead('Valya');
-const zombie = new Zombie('Oleg'); */
+import Bowman from "./bowman";
+import Swordsman from "./swordsman";
+import Magician from "./magician";
+import Daemon from "./daemon";
+import Undead from "./undead";
+import Zombie from "./zombie";
+
+const bowman = new Bowman("Ivan");
+const swordsman = new Swordsman("Yana");
+const magician = new Magician("Alexander");
+const daemon = new Daemon("Tanya");
+const undead = new Undead("Valya");
+const zombie = new Zombie("Oleg");
